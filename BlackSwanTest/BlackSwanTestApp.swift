1b33c7dda65fcf0c0f589af2d768d6de964dab8f
Maksim.Lamanskiy <lammax2012@gmail.com>
2021-06-09 13:52:33 +0300

    Initial Commit

---
+++ BlackSwanTest/BlackSwanTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackSwanTestApp.swift
+//  BlackSwanTest
+//
+//  Created by Максим Ламанский on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct BlackSwanTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
